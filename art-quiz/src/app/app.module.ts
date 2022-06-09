19a541dbb4aba6e0217152245304fd19d561b9c2
Yulia Lepner <ylepner@gmail.com>
2022-06-09 23:20:20 +0200

    feat: add settings-page component

--- art-quiz/src/app/app.module.ts
+++ art-quiz/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SettingsPageComponent } from './settings-page/settings-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    SettingsPageComponent
   ],
   imports: [
     BrowserModule,
