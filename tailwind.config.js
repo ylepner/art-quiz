7b21da376b5c5e13371cb0345ae6929f6a44fdfb
Yulia Lepner <ylepner@gmail.com>
2022-06-12 18:34:32 +0200

    style: add adaptive to main-page (520px)

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,14 @@ module.exports = {
       colors: {
         'whitesmoke': '#f5f5f5',
       },
+      screens: {
+        'sm': { 'max': '520px' },
+        // => @media (min-width: 640px and max-width: 767px) { ... }
+        'md': { 'min': '520px', 'max': '768px' },
+        // => @media (min-width: 1024px and max-width: 1279px) { ... }
+        'lg': { 'min': '768px' },
+        // => @media (min-width: 768px and max-width: 1023px) { ... }
+      },
     },
   },
   variants: {
@@ -21,3 +29,4 @@ module.exports = {
   },
   plugins: [],
 };
+
