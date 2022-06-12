215c9bf7f725f9e8f4e612df32799f5fe4c1243c
Yulia Lepner <ylepner@gmail.com>
2022-06-12 21:35:42 +0200

    style: change time buttons hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,11 +17,8 @@ module.exports = {
       },
       screens: {
         'sm': { 'max': '520px' },
-        // => @media (min-width: 640px and max-width: 767px) { ... }
         'md': { 'min': '520px', 'max': '768px' },
-        // => @media (min-width: 1024px and max-width: 1279px) { ... }
         'lg': { 'min': '768px' },
-        // => @media (min-width: 768px and max-width: 1023px) { ... }
       },
     },
   },
