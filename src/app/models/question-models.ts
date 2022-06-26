8efc1cc199dd310d6f60f751ec102c9309d7b958
Yulia Lepner <ylepner@gmail.com>
2022-06-26 23:45:20 +0200

    feat: update Question interface

--- src/app/models/question-models.ts
+++ src/app/models/question-models.ts
@@ -1,8 +1,15 @@
-export interface Question {
+export interface QuestionArtists {
   timer?: number,
   title: string,
   img: string,
   number: number,
+  answers: string[],
+  correctAnswer: number
+}
+
+export interface QuestionPictures {
+  timer?: number,
+  title: string,
   answers: string[],
   correctAnswer: number
 }
