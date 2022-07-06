21ee9585f714c4292c81900e7e3e48bf28634e62
Yulia Lepner <ylepner@gmail.com>
2022-07-07 00:42:52 +0200

    feat: provide data to quiz-pictures-page

--- src/app/quiz-pictures-page/quiz-pictures-page.component.ts
+++ src/app/quiz-pictures-page/quiz-pictures-page.component.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
+import { QuestionPictures } from '../models/question-models';
+import { PicturesService } from '../pictures.service';
+import { ResultsService } from '../results.service';
 
 @Component({
   selector: 'app-quiz-pictures-page',
   templateUrl: './quiz-pictures-page.component.html',
   styleUrls: ['./quiz-pictures-page.component.scss']
 })
-export class QuizPicturesPageComponent implements OnInit {
+export class QuizPicturesPageComponent {
 
-  constructor() { }
+  quizNumber = 0
+  questions: QuestionPictures[] = []
+  currentQuestionNumber = 0
 
-  ngOnInit(): void {
+  questions$ = this.route.params.pipe(
+    map((params) => {
+      this.quizNumber = Number(params['id'])
+      return this.pictureService.getPicturesGame(Number(params['id']))
+    }),
+  )
+
+  constructor(
+    private route: ActivatedRoute,
+    private pictureService: PicturesService,
+    private resultsService: ResultsService,
+    private dialog: MatDialog,
+  ) {
+    this.questions$.subscribe((questions) => {
+      this.questions = questions
+    })
+  }
+
+  get question() {
+    return this.questions[this.currentQuestionNumber]
   }
 
 }
