577ac1bdc009c7d45f853c6a9edbd9a5c9118874
Yulia Lepner <ylepner@gmail.com>
2022-08-05 21:20:57 +0200

    feat: update link to next quiz

--- src/app/quiz-artists-page/quiz-artists-page.component.ts
+++ src/app/quiz-artists-page/quiz-artists-page.component.ts
@@ -8,11 +8,14 @@ import { MatDialog } from '@angular/material/dialog';
 import { QuizResultsDialogComponent } from '../quiz-results-dialog/quiz-results-dialog.component';
 import { AnswerResult } from '../models/quiz-results';
 import { ResultsService } from '../results.service';
+
+const NUMBER_OF_QUIZZES = 11
 @Component({
   selector: 'app-quiz-artists-page',
   templateUrl: './quiz-artists-page.component.html',
   styleUrls: ['./quiz-artists-page.component.scss']
 })
+
 export class QuizArtistsPageComponent {
   quizNumber = 0
   currentQuestionNumber = 0
@@ -96,19 +99,23 @@ export class QuizArtistsPageComponent {
     const dialogRef = this.dialog.open(QuizResultsDialogComponent, {
       data: {
         correctAnswersNumber: this.correctAnswers,
-        hasMoreQuizzes: this.quizNumber !== 11,
-        quizNumber: this.quizNumber,
+        hasMoreQuizzes: this.quizNumber !== NUMBER_OF_QUIZZES,
+        quizNumber: this.getQuizNumber(),
         quizName: 'artists'
       }
     })
     dialogRef.afterClosed().subscribe(() => {
-      if (this.quizNumber <= 11) {
-        this.quizNumber += 1
-        this.currentQuestionNumber = 0
-        this.selectedAnswerNumber = undefined
-        this.correctAnswers = 0
-      }
+      this.currentQuestionNumber = 0
+      this.selectedAnswerNumber = undefined
+      this.correctAnswers = 0
     })
+  }
+
+  getQuizNumber() {
+    if (this.quizNumber !== NUMBER_OF_QUIZZES) {
+      return this.quizNumber + 1
+    }
+    return this.quizNumber
   }
 
   updateQuestionResults(questionNumber: number, isCorrect: boolean) {
