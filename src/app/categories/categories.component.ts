157ff86a541caca87ae5a93db6c7c5cc4ef59ab6
Yulia Lepner <ylepner@gmail.com>
2023-04-24 13:01:35 +0200

    feat: translate categories page

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { CategoryItem } from '../models/categories-models';
 import { PicturesService } from '../pictures.service';
+import { TranslateService } from '@ngx-translate/core';
+import { SettingsService } from '../settings.service';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -12,12 +14,19 @@ export class CategoriesComponent {
   type$ = this.route.params.pipe(
     map((params) => params['type']),
   )
+  language = ''
 
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private service: PicturesService
+    private service: PicturesService,
+    private translate: TranslateService,
+    private settingsService: SettingsService
   ) {
+    translate.addLangs(['en', 'ru']);
+    translate.setDefaultLang('en');
+    this.language = this.settingsService.getLanguage()
+    this.translate.use(this.language);
 
   }
 
